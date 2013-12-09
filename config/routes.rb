2ed7cbb9fe7ca381f2c8bacfaf992cbdeea72570
PatoBeltran <pbelsal@gmail.com>
2013-12-09 14:35:34 -0600

    add artículos prescritos feature

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ BDPed::Application.routes.draw do
     resources :historialmedicos
     resources :vacunas
   end
-  resources :citas
+  resources :citas do
+    resources :articulosprescritos
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
