dfae4a7c8aa377fe8a01626e51b717fea161560d
PatoBeltran <pbelsal@gmail.com>
2013-12-08 20:08:17 -0600

    assistants can view patients

--- app/controllers/assistants_controller.rb
+++ app/controllers/assistants_controller.rb
@@ -1,4 +1,7 @@
 class AssistantsController < ApplicationController
+  before_filter :has_to_authenticate
+  before_filter :just_doctor
+
   def new
     @assistant = Assistant.new
   end
@@ -6,15 +9,15 @@ class AssistantsController < ApplicationController
   def create
     @assistant = Assistant.new(assistant_params)
     begin
-      id= ActiveRecord::Base.connection.execute("SELECT COUNT(*) FROM assistants")
+      id= ActiveRecord::Base.connection.execute("SELECT COUNT(*) FROM assistants;")
       id.each do |row|
         id = row
       end
       id = id[0].to_i
-      raw_sql = "INSERT INTO assistants (aid, name, mail, password_digest, doc_id) VALUES ('#{id}', '#{@assistant.name}', '#{@assistant.mail}', '#{@assistant.password_digest}', '#{current_user.u_id}')"
+      raw_sql = "INSERT INTO assistants (aid, name, mail, password_digest, doc_id) VALUES ('#{id}', '#{@assistant.name}', '#{@assistant.mail}', '#{@assistant.password_digest}', '#{current_user.u_id}');"
       ActiveRecord::Base.connection.execute raw_sql
 
-      redirect_to root_url, notice: "Your assistant has been signed up!"
+      redirect_to pacientes_url, notice: "Your assistant has been signed up!"
     rescue
       render :new
     end
